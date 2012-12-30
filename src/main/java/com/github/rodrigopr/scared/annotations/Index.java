00febc225aacc892f8e6bd5029a978d6fb0e7974
Rodrigo Ribeiro <rodriguinho@jusbrasil.com.br>
2012-12-30 16:39:03 -0300

    Removing wrong template header from .java file

--- src/main/java/com/github/rodrigopr/scared/annotations/Index.java
+++ src/main/java/com/github/rodrigopr/scared/annotations/Index.java
@@ -5,17 +5,6 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-/**
- * <p>
- * Goshme Soluções para a Internet LTDA<br />
- * Projeto JusBrasil<br />
- * <a href="http://www.jusbrasil.com.br/">http://www.jusbrasil.com.br/</a>
- * </p>
- *
- * @author <a href="mailto:rodriguinho@jusbrasil.com.br>Rodrigo Pereira Ribeiro</a>
- * @since 28/12/2012
- */
-
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ ElementType.TYPE, ElementType.ANNOTATION_TYPE })
 public @interface Index {
